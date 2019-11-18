78ffce601cf43a9024be3a6bddf7722a3dcc66d3
Phuong <phuong@nguyen-viet>
2019-11-18 22:19:07 +0700

    add ssl

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,12 @@
 import * as ip_location from './infrastructure/ip_location';
 import express from 'express';
+import https from 'https';
+import fs from 'fs';
+
+const options = {
+  key: fs.readFileSync(process.env.KEY_PATH),
+  cert: fs.readFileSync(process.env.CERT_PATH)
+};
 
 const ipLocationService = new ip_location.MmdbLocationService();
 
@@ -10,12 +17,17 @@ const ip2code = async (ip: string) => {
 // Create Express server
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send(req.ips);
+app.get("/", async (req, res) => {
+  const ip = req.headers['x-real-ip'].toString();
+  const code = await ip2code(ip);
+  res.send({
+    ip,
+    code
+  });
 });
 
-const server = app.listen(3000, () => {
-  console.log('OK');
-});
+app.listen(3001);
+
+const server = https.createServer(options, app).listen(3000);
 
 export default server;
